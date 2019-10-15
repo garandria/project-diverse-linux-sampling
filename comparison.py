0bcc425b7845b207d5f0deee38a71903032e2de8
ngahy-be <georgesaaron.randrianaina.etu@univ-lille.fr>
2019-10-15 16:35:45 +0200

    feat(comparison): option to clean the feature from dimacs file

--- comparison.py
+++ comparison.py
@@ -2,34 +2,35 @@
 '''
 
 TUXML_FILENAME='alloptions.csv'
-DIMACS_FILENAME='out.dimacs'
+DIMACS_FILENAME='../Feature-Model-History-of-Linux/2017/2017-09-11T13_10_57-07_00/2017/2017-09-11T13_10_57-07_00/out.dimacs'
 
-def csv_reader(filename, column, sep=';', comment='#'):
+def csv_reader(filename, column=1, sep=';', comment='#'):
     '''
     '''
     res = set()
     col = column - 1
-    assert(col > 0, 'Negative column number')
     with open(filename, 'r') as stream:
-        line = readline()
-        while not line:
+        stream.readline()       # first line (title)
+        line = stream.readline()        
+        while line:
             if not (line[0] == comment):
                 feature = line.split(sep)[col]
                 res.add(feature)
-            line = readline()
+            line = stream.readline()
     return res
 
 
-def dimacs_reader(filename):
+def dimacs_reader(filename, clean=False):
     '''
     '''
     res = set()
     with open(filename, 'r') as stream:
-        line = readline()
-        while not line:
-            if line[0] == 'c':
-                # c 666 FEATURE_NAME
-                res.add(line.split(' ')[2])
-        line = readline()
+        line = stream.readline()
+        while line[0] == 'c':
+            # c 666 FEATURE_NAME
+            feature = line.split(' ')[2].split('\n')[0]
+            if clean:
+                feature = feature.split('=')[0]
+            res.add(feature)
+            line = stream.readline()
     return res
-
