44e41ee2c20996b2060e153c050066c5bcb6cb96
ngahy-be <georgesaaron.randrianaina.etu@univ-lille.fr>
2019-10-15 16:41:21 +0200

    feat(comparison): command line option & file generation

--- comparison.py
+++ comparison.py
@@ -1,5 +1,6 @@
 '''
 '''
+import argparse
 
 TUXML_FILENAME='alloptions.csv'
 DIMACS_FILENAME='../Feature-Model-History-of-Linux/2017/2017-09-11T13_10_57-07_00/2017/2017-09-11T13_10_57-07_00/out.dimacs'
@@ -34,3 +35,30 @@ def dimacs_reader(filename, clean=False):
             res.add(feature)
             line = stream.readline()
     return res
+
+def main():
+    '''
+    '''
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--clean',
+                        help='clean the name in the dimacs file',
+                        action='store_true')
+    args = parser.parse_args()
+
+    diff = set()
+    csv = csv_reader("../Kanalyser/alloptions-x64-v4.15.csv", sep=',')
+    dimacs = dimacs_reader(DIMACS_FILENAME, args.clean)
+    len_csv = len(csv)
+    len_dimacs = len(dimacs)
+    if len_dimacs > len_csv:
+        diff = dimacs - csv
+    else:
+        diff = csv - dimacs
+    with open('output.csv', 'w') as stream:
+        stream.write('# CSV FILE : {} features\n'.format(len_csv))
+        stream.write('# DIMACS FILE : {} features\n'.format(len_dimacs))
+        for f in diff:
+            stream.write('{}\n'.format(f))
+    
+if __name__ == '__main__':
+    main()
