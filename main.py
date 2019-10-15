6b8aa41ea645b5bac608f02ae503a54f14a77994
ngahy-be <georgesaaron.randrianaina.etu@univ-lille.fr>
2019-10-15 17:01:09 +0200

    feat(main): read returns a set now

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import os
-import sys
 
-cmd = "make randconfig"
-
-def usage():
-    print("python3 main.py [.config]file n")
-    print("n\t number of tests")
-    
-def read(filename, data):
+def read(filename):
+    data = set()
     with open(filename, 'r') as stream:
         line = stream.readline()
         while line != '':
@@ -18,27 +11,4 @@ def read(filename, data):
                 except KeyError:
                     data[res] = 1
             line = stream.readline()
-
-
-def main():
-    if len(sys.argv) >= 1:
-        data = dict()
-        n = int(sys.argv[1])
-        for i in range(n):
-            os.system(cmd)
-            read(".config", data)
-
-        with open('data.csv', 'w') as stream:
-            stream.write('{:64s};{}\n'.format('feature', 'occurence'))
-            for k in data:
-                stream.write('{:64s};{:d}\n'.format(k, data[k]))
-    else :
-        usage()
-
-if __name__ == '__main__':
-    try:
-        main()
-    except ValueError:
-        print("Invalid size {}".format(sys.argv[-1]))
-        exit(0)
-            
+    return data
