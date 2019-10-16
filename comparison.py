4f0126190a19626733a0299a0c950c2aaf3d6d9d
ngahy-be <georgesaaron.randrianaina.etu@univ-lille.fr>
2019-10-16 09:25:00 +0200

    feat & doc(comparison): truncates file if file exists & file doc

--- comparison.py
+++ comparison.py
@@ -1,11 +1,35 @@
-'''
+'''This program compares a dimacs file (of linux configuration) and a
+csv file containing all options.
+
+For an example of dimacs file, see :
+https://github.com/PettTo/Feature-Model-History-of-Linux/tree/master/2017/2017-09-11T13_10_57-07_00
+
+For an example of a csv file with all options, see :
+https://github.com/TuxML/Kanalyser/blob/master/alloptions-x64-v4.15.csv
+
 '''
 
+__author__ = "Georges Aaron RANDRIANAINA"
+__email__ = "georges-aaron.randrianaina@ens-rennes.fr"
+
+# To parse the command line argument
 import argparse
 
 
 def csv_reader(filename, column=1, sep=';', comment='#'):
-    '''
+    '''Reads a csv file of options and return a set containing all options
+
+    :param filename: path (file included) to the csv file
+    :type: str
+    :param column: (optional) number of the column containing options
+    (default is 1.)
+    :type: int
+    :param sep: (optional) separator used in the file (default is ';')
+    :type: str
+    :param comment: (optional) character for comment (default is '#')
+    :type: str
+    :return: set of all options
+    :rtype: set
     '''
     res = set()
     col = column - 1
@@ -21,7 +45,15 @@ def csv_reader(filename, column=1, sep=';', comment='#'):
 
 
 def dimacs_reader(filename, clean=False):
-    '''
+    '''Reads a dimacs file and returns a set containing all the options
+
+    :param filename: path (file included) to the dimacs file
+    :type: str
+    :param clean: (optional) clean the option : get rid of extra '=' or '=n' or
+    ...  added the option (default is False)
+    :type: bool
+    :return: set of options
+    :rtype: set
     '''
     res = set()
     with open(filename, 'r') as stream:
@@ -37,7 +69,12 @@ def dimacs_reader(filename, clean=False):
 
 
 def main():
-    '''
+    '''The main function that parse the input arguments from the command
+    line and use the previous defined function to read files. This
+    functoin generates a file containing the number of features in
+    each file (2 first line of the file) as comment then gives a list
+    of options that the files do not have in common.
+
     '''
     parser = argparse.ArgumentParser()
     parser.add_argument('--clean',
@@ -60,11 +97,16 @@ def main():
         diff = dimacs - csv
     else:
         diff = csv - dimacs
-    with open('output.csv', 'a') as stream:
-        stream.write('# CSV FILE : {} features\n'.format(len_csv))
-        stream.write('# DIMACS FILE : {} features\n'.format(len_dimacs))
-        for f in diff:
-            stream.write('{}\n'.format(f))
+
+    content = ''
+    content += '# CSV FILE : {} features\n'.format(len_csv)
+    content += '# DIMACS FILE : {} features\n'.format(len_dimacs)
+    content += '\n'
+    for f in diff:
+        content += '{}\n'.format(f)
+
+    with open('output.csv', 'w') as stream:
+        stream.write(content)
 
 
 if __name__ == '__main__':
