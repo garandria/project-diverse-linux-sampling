63e91ffa608142d4ad1ad7557a36ef0b7bbe35ee
ngahy-be <georges-aaron.randrianaina@ens-rennes.fr>
2020-01-22 19:36:40 +0100

    feat(extractor): add command line argument for files and directories

--- extractor.py
+++ extractor.py
@@ -3,9 +3,9 @@ import argparse
 from file import DimacsFile, CSVFile, Checker
 
 
-DIMACS = "../../kconfigreader/out.dimacs"
-CONFIG_DIR = "../../linux-4.14.152"
-CSV = "../../linux-4.14.152/alloptions-x86.4.14.152.csv"
+DIMACS = "../kconfigreader/out.dimacs"
+CONFIG_DIR = "../linux-4.14.152"
+CSV = "../linux-4.14.152/alloptions-x86.4.14.152.csv"
 CONF = ["tinyconfig", "randconfig"] #, "defconfig", "allnoconfig",\
 #        "allyesconfig", "allmodconfig", "alldefconfig"]
 
@@ -13,15 +13,21 @@ CONF = ["tinyconfig", "randconfig"] #, "defconfig", "allnoconfig",\
 def main():
     ''' main function '''
     parser = argparse.ArgumentParser()
+    parser.add_argument("--cdir", type=str, default=CONFIG_DIR,
+                        help="local directory of linux source code \
+                        (to launch the Makefile)")
+    parser.add_argument("--dimacs", type=str, default=DIMACS,
+                        help="dimacs file name")
+    parser.add_argument("--csv", type=str, default=CSV, help="all options csv file")
     parser.add_argument("-n", type=int, default=20,
                         help="no of iteration")
     args = parser.parse_args()
     out = "name,nb_yes,nb_mod,bool,tristate,both,last_clause,in\n"
     for i in range(args.n):
         for configuration in CONF:
-            os.system("make {} -C {}".format(configuration, CONFIG_DIR))
-            checker = Checker(DIMACS, CSV)
-            checker.set_dot_config_file("{}/.config".format(CONFIG_DIR))
+            os.system("make {} -C {}".format(configuration, args.cdir))
+            checker = Checker(args.dimacs, args.csv)
+            checker.set_dot_config_file("{}/.config".format(args.cdir))
             nb = checker.get_nb_tristate()
             res_bool = checker.check_bool()
             checker.clean()
