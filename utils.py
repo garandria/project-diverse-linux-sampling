b48d395a26114e3bc5dcafcc30af981e723ecac5
RANDRIANAINA Georges Aaron <georges-aaron.randrianaina@irisa.fr>
2021-02-20 19:58:03 +0100

    add(utils): Alloptions classe for CSV file

--- utils.py
+++ utils.py
@@ -56,3 +56,37 @@ class DimacsFla:
         for k, v in self.__dimacs.items():
             if v == "{}_MODULE".format(symbol):
                 return k
+
+class Alloptions:
+    """Alloptions class represents the csv of all options of a Linux Kernel.
+
+    Use Kanalyser (https://github.com/TuxML/Kanalyser/) to generate a csv file of
+    all symboles and their type.
+
+    :param alloptions: src csv file
+    :type alloptions: str
+    """
+
+    def __init__(self, alloptions):
+        """Constructor method"""
+        self.__alloptions = pd.read_csv(alloptions)
+
+    def get_options(self):
+        """Gives the dataframe containing options and their types
+        
+        :return: options and their types
+        :rtype: pd.Dataframe
+        """
+        return self.__alloptions
+    
+    def get_kconfig_type(self, symbol):
+        """Retrieve the type of a kconfig symbol
+
+        :param symbol: name of the option
+        :type symbol: str
+        :return: type of the option
+        :rtype: str
+        """
+        ktype = self.__alloptions\
+                    .query("option == " + '"' + option_name + '"')['type']
+        return ktype.values[0]
